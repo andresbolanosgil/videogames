0775f81fb49f381ce83978f632a2c7d73a86a461
andres <ricardoperezroberto@gmail.com>
2024-09-13 21:30:20 -0500

    message

---
+++ Scrips python/ejemplo.py
@@ -0,0 +1,15 @@
+#este es un comentario
+'''
+esto es un comentario bloque
+'''
+data='''
+esto es una variable string bloque'''
+n1=10
+n2=20
+suma=n1+n2
+print(suma)
+
+print("la suma es", suma)
+print(f"la suma es: {suma}")
+
+
---
+++ Scrips python/number_race/main.py
@@ -0,0 +1,7 @@
+'''escript description number race
+dev andres bolaños '''
+from random import randint
+dice=randint(1,6)
+dice2=randint(1,6)
+print(f"dice 1: {dice}")
+print(f"dice 2: {dice2}")
